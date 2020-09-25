2cb65a91b41e74129cadba7c31e59a621ac132fd
Nattida Wong <nattidac.wong@gmail.com>
2020-09-25 13:46:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,7 @@
 # Make your shoe class here!
 class Shoe
 
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
@@ -46,4 +45,5 @@ class Shoe
 
   def cobble
     puts "Your shoe is as good as new!"
+  end
 end
